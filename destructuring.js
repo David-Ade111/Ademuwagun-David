584b542bd2ae984a08f1a7d115be94cf58184797
David-Ade111 <davidadewole22@gmail.com>
2024-09-25 11:00:53 +0100

    Add files via upload

---
+++ destructuring.js
@@ -0,0 +1,37 @@
+class Person {
+    constructor(name, age, profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession || 'Unemployed';
+    }
+
+    getDetails = () => {
+        const { name, age, profession } = this; // Destructuring this
+        return `${name} is ${age} years old and works as a ${profession}.`;
+    };
+
+    changeProfession = (newProfession) => {
+        if (newProfession) {
+            this.profession = newProfession;
+        }
+    };
+
+    // Static method to get details of multiple persons using map
+    static getAllDetails(persons) {
+        return persons.map(person => person.getDetails());
+    }
+}
+
+// Create instances of the Person class using destructuring in the constructor
+const person1 = new Person('Daniel', 30, 'Software Programmer');
+const person2 = new Person('Johnson', 25, 'Graphic Designer');
+const person3 = new Person('Micheal', 21, 'Data Analyst');
+
+// Store persons in an array
+const personsArray = [person1, person2, person3];
+
+// Use the static method to get details of all persons
+const allDetails = Person.getAllDetails(personsArray);
+
+// Log the details of all persons
+console.log(allDetails);
---
+++ rectangle.js
@@ -0,0 +1,8 @@
+function rectangleArea (length, breath) {
+    let rectangleArea = length * breath; 
+
+    return rectangleArea
+}
+
+console.log (rectangleArea (6, 5))
+console.log (rectangleArea (12,6))
